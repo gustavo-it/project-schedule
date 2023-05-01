3a4239f00d2f6ce5a6eb639752baa886c62eb985
gustavo-it <gustavo_castrosilva@yahoo.com>
2023-05-01 14:40:39 -0300

    Cadastrando contatos (Create)

--- routes.js
+++ routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const route = express.Router();
-const homeController = require('./src/controllers/homeController')
-const loginController = require('./src/controllers/loginController')
+const homeController = require('./src/controllers/homeController');
+const loginController = require('./src/controllers/loginController');
+const contatoController = require('./src/controllers/contatoController');
+const { loginRequired } = require('./src/middlewares/middleware');
 
 // Rotas home
 route.get('/', homeController.index);
@@ -11,5 +13,8 @@ route.get('/login/index', loginController.index);
 route.post('/login/register', loginController.register);
 route.post('/login/login', loginController.login);
 route.get('/login/logout', loginController.logout);
+
+// Rotas de contato
+route.get('/contato/index', loginRequired ,contatoController.index)
 
 module.exports = route;
