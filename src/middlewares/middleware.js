b38edd0ebd0f849edc02ece492556e2034ab13e0
gustavo-it <gustavo_castrosilva@yahoo.com>
2023-05-01 11:21:09 -0300

    Criando usuários (1)

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,7 +1,6 @@
 exports.middlewareGlobal = (req, res, next) => {
-    if(req.body.cliente) {
-        console.log(`Vi que você postou ${req.body.cliente}`)
-    };
+    res.locals.errors = req.flash('errors');
+    res.locals.success = req.flash('success');
     next();
 };
 
---
+++ src/models/LoginModel.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const LoginSchema = new mongoose.Schema({
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+});
+
+const LoginModel = mongoose.model('Login', LoginSchema);
+
+class Login {
+    constructor(body) {
+        this.body = body;
+        this.errors = []; // se tiver algum erro aqui dentro n posso cadastrar
+        this.user = null;
+    };
+
+    async register() {
+        this.valida();
+        if(this.errors.length > 0) return;
+        // Se o usuário for criado é possível acessar de fora
+        try {
+            this.user = await LoginModel.create(this.body);
+        } catch(error) {
+            console.log(error);
+        }
+    };
+
+    valida() {
+        // Validação dos campos
+        // e-mail precisa ser válida
+        // senha precisa ter entre 3 e 14 caracteres
+        this.cleanUp();
+
+        if(!validator.isEmail(this.body.email)) this.errors.push('E-mail inválido');
+        if(this.body.password.length < 3 || this.body.password.length > 50) {
+            return this.errors.push('A senha precisa ter entre 3 e 50 caracteres');
+        }
+    };
+
+    cleanUp() {
+        // vai garantir que tudo dentro de body é uma string
+        for(const key in this.body) {
+            if(typeof this.body[key] !== 'string') {
+                this.body[key] = '';
+            };
+        };
+
+        // Garantindo que o objeto tenha somente estes campos
+        this.body = {
+            email: this.body.email,
+            password: this.body.password,
+        };
+    };
+};
+
+module.exports = Login;
